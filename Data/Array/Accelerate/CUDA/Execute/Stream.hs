ae8e46df88f05d9936dc694cebf4d87d06bbbcfb
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2013-11-21 00:05:03 +1100

    use finalisers to clean up concurrent execution streams

--- Data/Array/Accelerate/CUDA/Execute/Stream.hs
+++ Data/Array/Accelerate/CUDA/Execute/Stream.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns  #-}
+{-# LANGUAGE MagicHash     #-}
+{-# LANGUAGE UnboxedTuples #-}
 -- |
 -- Module      : Data.Array.Accelerate.CUDA.Execute.Stream
 -- Copyright   : [2013] Manuel M T Chakravarty, Gabriele Keller, Trevor L. McDonell
@@ -25,17 +27,18 @@ import qualified Data.Array.Accelerate.CUDA.FullList            as FL
 import qualified Data.Array.Accelerate.CUDA.Debug               as D
 
 -- libraries
-import Control.Monad                                            ( void )
 import Control.Monad.Trans                                      ( MonadIO, liftIO )
 import Control.Exception                                        ( bracket_ )
-import Control.Concurrent                                       ( forkIO )
 import Control.Concurrent.MVar                                  ( MVar, newMVar, withMVar, mkWeakMVar )
 import System.Mem.Weak                                          ( Weak, deRefWeak )
-import Foreign.CUDA.Driver.Stream                               ( Stream )
+import Foreign.CUDA.Driver.Stream                               ( Stream(..) )
 import qualified Foreign.CUDA.Driver                            as CUDA
 import qualified Foreign.CUDA.Driver.Stream                     as Stream
 
 import qualified Data.HashTable.IO                              as HT
+
+import GHC.Base
+import GHC.Ptr
 
 
 -- Representation
@@ -59,12 +62,11 @@ data Reservoir          = Reservoir {-# UNPACK #-} !RSV
 --
 {-# INLINE streaming #-}
 streaming :: MonadIO m => Context -> Reservoir -> (Stream -> m a) -> m (Event, a)
-streaming !ctx !rsv@(Reservoir !_ !weak_rsv) !action = do
+streaming !ctx !rsv !action = do
   stream <- liftIO $ create ctx rsv
   result <- action stream
   end    <- liftIO $ Event.waypoint stream
-  liftIO $ merge (weakContext ctx) weak_rsv stream
-  return $ (end, result)
+  return (end, result)
 
 
 -- Primitive operations
@@ -86,22 +88,25 @@ new = do
 --
 {-# INLINE create #-}
 create :: Context -> Reservoir -> IO Stream
-create !ctx (Reservoir !ref _) = withMVar ref $ \tbl -> do
+create !ctx (Reservoir !ref !weak_rsv) = withMVar ref $ \tbl -> do
   let key = deviceContext ctx
   --
-  ms    <- HT.lookup tbl key
-  case ms of
+  old    <- HT.lookup tbl key
+  stream <- case old of
     Nothing -> do
-        stream <- Stream.create []
-        message ("new " ++ showStream stream)
-        return stream
+      stream <- Stream.create []
+      message ("new " ++ showStream stream)
+      return stream
 
     Just (FL () stream rest) -> do
       case rest of
         FL.Nil          -> HT.delete tbl key
         FL.Cons () s ss -> HT.insert tbl key (FL () s ss)
-      --
+        --
       return stream
+  --
+  addStreamFinalizer stream $ merge (weakContext ctx) weak_rsv stream
+  return stream
 
 
 -- Merge a stream back into the reservoir. This is done asynchronously, once all
@@ -109,29 +114,34 @@ create !ctx (Reservoir !ref _) = withMVar ref $ \tbl -> do
 --
 {-# INLINE merge #-}
 merge :: Weak CUDA.Context -> Weak RSV -> Stream -> IO ()
-merge !weak_ctx !weak_rsv !stream
-  = void . forkIO
-  $ do  -- wait for all operations to complete
-        -- Stream.block stream
+merge !weak_ctx !weak_rsv !stream = do
+  -- wait for all preceding operations submitted to the stream to complete
+  Stream.block stream
 
-        -- Now check whether the context and reservoir are still active. Return
-        -- the stream back to the reservoir for later reuse if we can, otherwise
-        -- destroy it.
-        --
-        mc <- deRefWeak weak_ctx
-        case mc of
-          Nothing       -> message ("finalise/dead context " ++ showStream stream)
-          Just ctx      -> do
-            --
-            mr <- deRefWeak weak_rsv
-            case mr of
-              Nothing   -> trace ("merge/free " ++ showStream stream) $ Stream.destroy stream
-              Just ref  -> trace ("merge/save " ++ showStream stream) $ withMVar ref $ \tbl -> do
-                --
-                ms <- HT.lookup tbl ctx
-                case ms of
-                  Nothing       -> HT.insert tbl ctx (FL.singleton () stream)
-                  Just ss       -> HT.insert tbl ctx (FL.cons () stream ss)
+  -- Now check whether the context and reservoir are still active. Return
+  -- the stream back to the reservoir for later reuse if we can, otherwise
+  -- destroy it.
+  mc <- deRefWeak weak_ctx
+  case mc of
+    Nothing       -> message ("finalise/dead context " ++ showStream stream)
+    Just ctx      -> do
+      --
+      mr <- deRefWeak weak_rsv
+      case mr of
+        Nothing   -> trace ("merge/free " ++ showStream stream) $ Stream.destroy stream
+        Just ref  -> trace ("merge/save " ++ showStream stream) $ withMVar ref $ \tbl -> do
+          --
+          ms <- HT.lookup tbl ctx
+          case ms of
+            Nothing       -> HT.insert tbl ctx (FL.singleton () stream)
+            Just ss       -> HT.insert tbl ctx (FL.cons () stream ss)
+
+
+-- Add a finaliser to an execution stream
+--
+addStreamFinalizer :: Stream -> IO () -> IO ()
+addStreamFinalizer st@(Stream (Ptr st#)) f = IO $ \s ->
+  case mkWeak# st# st f s of (# s', _w #) -> (# s', () #)
 
 
 -- Destroy all streams in the reservoir.
